87cb1f4b405ea51592fc21fb1955e5b40ffb5cf0
michaelscl <michaelscz@gmail.com>
2023-02-02 11:06:01 +0100

    final check_readnumberfromurl

--- bin/check_readnumberfromurl.sh
+++ bin/check_readnumberfromurl.sh
@@ -8,21 +8,28 @@ URL=$1
 WARN=$2
 CRIT=$3
 
-POCET=`wget -O - --no-check-certificate --quiet $URL`
+POCET=`wget --user-agent="Mozilla" --no-check-certificate -O - $URL`
 if [ $? -ne 0 ]; then
     echo "Nepodarilo se stahnout hodnotu z $URL"
-    exit 2
+    exit 3
 fi
 
-echo "Pocet: $POCET"
+if [ ${#POCET} -lt 1 ]; then
+    echo "Nepodarilo se stahnout hodnotu z $URL - prazdna hodnota"
+    exit 3
+elif [ ${#POCET} -gt 6 ]; then
+    echo "Nepodarilo se stahnout hodnotu z $URL - vracena hodnota neni cislo"
+    exit 3
+fi
 
-if [ $POCET -gt $CRIT ]		# critical
+echo "Pocet: ${POCET}"
+
+if [ $POCET -ge $CRIT ]         # critical
 then
     exit 2
-elif [ $POCET -gt $WARN ]	# warn
+elif [ $POCET -ge $WARN ]       # warn
 then
     exit 1
-else
+else                            # ok
     exit 0
 fi
-
